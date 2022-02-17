0c66b1b9c0e5981433505dcc5e2a9252dba4594a
Suwijak Promsatid <suwijak.pak@gmail.com>
2022-02-17 11:54:19 +0700

    v.1

---
+++ index.js
@@ -0,0 +1,22 @@
+let numberInput = document.getElementById('form');
+let runButton = document.getElementById('search');
+let output = document.getElementById('answer');
+
+function printMultiply() {
+  let number = Number(numberInput.value);
+  let outputHtml = '';
+  if (number === 0) {
+    output.innerHTML = 'อยากบอกว่าเสียใจ';
+    return;
+  }
+  for (let i = 1; i <= 12; i++) {
+    outputHtml += '<p>';
+    outputHtml += number + ' x ' + i + ' = ' + (number*i);
+    outputHtml += '</p>';
+  }
+  output.innerHTML = outputHtml;
+}
+
+runButton.addEventListener('click', printMultiply);
+
+console.log('เพิ่มข้อมูลตำแหน่งแรก')
